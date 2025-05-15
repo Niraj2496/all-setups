71239f069aee5c89b27660e2d1373e6b5a93736d
Niraj Banayya Ganjiwar <adinirganjiwar143@gmail.com>
2025-05-15 15:49:13 +0530

    Update arango.sh

--- arango.sh
+++ arango.sh
@@ -1,8 +1,7 @@
-echo 'deb https://download.arangodb.com/arangodb34/DEBIAN/ /' | sudo tee /etc/apt/sources.list.d/arangodb.list
-wget -q https://download.arangodb.com/arangodb34/DEBIAN/Release.key -O- | sudo apt-key add -
-sudo apt update -y
-sudo apt -y install apt-transport-https
-sudo apt -y install arangodb3
+echo 'deb https://download.arangodb.com/arangodb312/DEBIAN/ /' | sudo tee /etc/apt/sources.list.d/arangodb.list
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install arangodb3=3.12.4.3-1
 sudo systemctl start arangodb3
 sudo systemctl status arangodb3
 sudo arangosh
